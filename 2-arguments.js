95116cc804fb48eb30d21eafa8a6ff6239b69581
quadritech <96469908+quadritech@users.noreply.github.com>
2025-05-27 19:20:17 +0100

    commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -9,23 +9,3 @@ if (num.length === 0){
 }else {
     console.log("Arguments found");
 }
-
-let arg = process.argv.slice(1);
-
-if (arg.length === 0) {
-    console.log("No argument");
-} else if (arg.length === 1) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
-
-let arg1 = process.argv.slice(2);
-
-if (arg1.length === 0) {
-    console.log("No argument");
-} else if (arg1.length === 1) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
