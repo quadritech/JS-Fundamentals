a7c7e90af0095e731741ea55a09e63477357468e
quadritech <96469908+quadritech@users.noreply.github.com>
2025-05-27 20:12:37 +0100

    printing argument

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,8 @@
-//This is a special array in Node.js that holds all the command-line arguments and slice is used to skip the first 2 arg
-
 let num = process.argv;
 
-if (num.length === 0){
+if (num.length === 0) {
     console.log("No argument");
-}else if (num.length === 1){
+}else if (num.length === 1) {
     console.log("Argument found");
 }else {
     console.log("Arguments found");
