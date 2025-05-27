179bc1de6dd2937eb996f0216f3c1b6b34d90ffd
quadritech <96469908+quadritech@users.noreply.github.com>
2025-05-27 18:37:09 +0100

    printing arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -10,17 +10,22 @@ if (num.length === 0){
     console.log("Arguments found");
 }
 
+let arg = process.argv.slice(1);
 
-/*const printArgumentsMessage = () => {
-    const numArgs = process.argv.length - 2; // Subtract 2 to exclude node and script path
+if (arg.length === 0) {
+    console.log("No argument");
+} else if (arg.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
 
-    if (numArgs === 0) {
-        console.log("No argument");
-    } else if (numArgs === 1) {
-        console.log("Argument found");
-    } else {
-        console.log("Arguments found");
-    }
-};
+let arg1 = process.argv.slice(2);
 
-printArgumentsMessage();*/
+if (arg1.length === 0) {
+    console.log("No argument");
+} else if (arg1.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
