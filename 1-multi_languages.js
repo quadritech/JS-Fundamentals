6eecba7764b7d38c03ceb87fe120f94e74c131a6
quadritech <96469908+quadritech@users.noreply.github.com>
2025-05-27 15:16:15 +0100

    printing 3linees scripts

---
+++ 1-multi_languages.js
@@ -0,0 +1,13 @@
+/* 
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+*/
+
+let scrpits = "C is fun"
+let scrpits1 = "python is cool"
+let scrpits2 = "JavaScript is amazing"
+
+console.log (scrpits);
+console.log(scrpits1);
+console.log(scrpits2);
