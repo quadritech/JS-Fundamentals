de0d73d240bb10000d45408311c74cef1e012770
quadritech <96469908+quadritech@users.noreply.github.com>
2025-05-28 04:44:15 +0100

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+    console.log(1);
+} else {
+    console.log(factorial(num));
+}
